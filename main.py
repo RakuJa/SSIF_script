e15e235b43e32b9208e582255bb4e3ee5d6ebdd3
RakuJa <danielegiachetto1999@gmail.com>
2021-04-05 13:31:49 +0200

    Add return list

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-def check(file_name, string_to_search):
+def check(file_name, string_to_search) -> list:
+    to_return = []
     with open(file_name, 'r', encoding='utf-8') as f:
         for line in f:
             if line.find(string_to_search) != -1:
-                print(line)
-    # return open(file_name, 'r', encoding='utf-8').read().find(string_to_search)
+                to_return.append(line)
+    return to_return
 
 
 if __name__ == "__main__":
     #file_name = input("Enter file path")
     #to_search = input("String to search in file")
     file_name = "0.txt"
-    to_search = "Sara"
+    to_search = "Nome:Cognome"
     print(check(file_name, to_search))
 
 
