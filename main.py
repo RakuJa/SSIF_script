d69bbab096518be5b2485b5f138cf2173fa9ee90
RakuJa <danielegiachetto1999@gmail.com>
2021-04-05 13:57:21 +0200

    Shows occurences once search is over, to fix

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def check(file_name, string_to_search) -> list:
                 to_return.append(line)
     return to_return
 
-def check_file_validity(file_name):
+def check_file_validity(file_name) -> bool:
     if file_name == "":
         return False
     with open(file_name, 'r', encoding='utf-8'):
@@ -18,17 +18,24 @@ if __name__ == "__main__":
     keep_taking_input = True
     file_list =[]
     while keep_taking_input:
-        file_name = input("Enter file path, Empty line to stop input")
+        file_name = input("Enter file path, Empty line to stop input ")
         keep_taking_input = check_file_validity(file_name)
         if keep_taking_input:
             file_list.append(file_name)
             
-    to_search = input("Enter string to search")
+    to_search = input("Enter string to search ")
     print ("\n Search started.... \n")
+    occurrences_list = []
     for file_name in file_list:
         print("\n Starting search in " + file_name + "\n")
-        print(check(file_name, to_search))
+        tmp = check(file_name, to_search)
+        occurrences_list.append(tmp)
+        print(tmp)
         print("\n" + file_name + "Searched...." + "\n")
     print("\n" + "Search over........." + "\n")
+
+    print( "Found " + str(len(occurrences_list)) + " Occurances of string")
+    print(" \n Here's the list : \n")
+    print(occurrences_list)
 
 
