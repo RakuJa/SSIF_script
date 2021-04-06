927d947e6c0eca38d4601a1d6c7626d5e5eedff7
RakuJa <danielegiachetto1999@gmail.com>
2021-04-06 22:00:24 +0200

    Patch 2.0, Add pdf search functionality, clean up code, split the open txt function from the search function

--- main.py
+++ main.py
@@ -6,13 +6,61 @@ from os import listdir
 from os.path import isfile, join
 from contextlib import redirect_stdout
 
-def check(file_name, string_to_search) -> list:
+
+def pdf_to_text(file_name) -> str:
+    from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+    from pdfminer.pdfpage import PDFPage
+    from pdfminer.converter import XMLConverter, HTMLConverter, TextConverter
+    from pdfminer.layout import LAParams
+    import io
+    
     to_return = []
-    with open(file_name, 'r', encoding='utf-8') as f:
-        for line in f:
-            if line.find(string_to_search) != -1:
-                to_return.append(line)
+    fp = open(file_name, 'rb')
+    rsrcmgr = PDFResourceManager()
+    retstr = io.StringIO()
+    codec = 'utf-8'
+    laparams = LAParams()
+    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+    # Create a PDF interpreter object.
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    # Process each page contained in the document.
+
+    for page in PDFPage.get_pages(fp):
+        interpreter.process_page(page)
+        data =  retstr.getvalue()
+
+    return data
+
+def parse_pdf(file_name, string_to_search) -> list:
+    data = pdf_to_text(file_name)
+    tmp_file = file_name + ".txt"
+    write_info_to_file("", data, tmp_file, "")
+    tmp = parse_txt(tmp_file, string_to_search)
+    os.remove(tmp_file)
+    return tmp
+    
+    
+
+def parse_txt_data(data, string_to_search) -> list:
+    to_return = []
+    for line in data:
+        if line.find(string_to_search) != -1:
+            to_return.append(line)
     return to_return
+
+def parse_txt(file_name, string_to_search) -> list:
+    with open(file_name, 'r', encoding='utf-8') as f:
+        data = f.readlines()
+        tmp = parse_txt_data(data, string_to_search)
+        return tmp
+
+
+def check_extension(file_name, string_to_search) -> list:
+    if file_name.lower().endswith('.txt'):
+        return parse_txt(file_name, string_to_search)
+    if file_name.lower().endswith('.pdf'):
+        return parse_pdf(file_name, string_to_search)
+    
 
 def check_file_validity(file_name) -> bool:
     with open(file_name, 'r', encoding='utf-8'):
@@ -44,10 +92,10 @@ def take_file_input() -> list:
     file_list = remove_duplicates(file_list)
     return file_list
 
-def write_info_to_file(info_string, flattened_list) -> bool:
-    with open("results.txt", 'a') as f:
+def write_info_to_file(info_string, flattened_list,file_name = "results.txt", separator = ' \n') -> bool:
+    with open(file_name, 'a') as f:
         print(info_string, file=f)
-        print(*flattened_list, sep=' \n', file=f)
+        print(*flattened_list, sep=separator, file=f)
 
 def print_tutorial():
     print("This script will search for a string inside a list of files (1..n) \n")
@@ -66,9 +114,7 @@ def print_tutorial():
             sys.exit()
         if answer == "Y":
             return;
-          
-    
-        
+            
     
 if __name__ == "__main__":
 
@@ -91,15 +137,16 @@ if __name__ == "__main__":
     for file_name in file_list:
         if verbose :
             print("\n Starting search in " + file_name + "\n")
-        tmp = check(file_name, to_search)
-        occurrences_list.append(tmp)
+        tmp = check_extension(file_name, to_search)
+        if tmp is not None:
+            occurrences_list.append(tmp)
         if verbose :
             print("\n" + file_name + " Searched...." + "\n")
     if verbose :
         print("\n" + "Search over........." + "\n")
 
     flattened_list = list(chain(*occurrences_list))
-    prepare_string = "Scanned " + str(len(occurrences_list)) + " files \n" + "Found " + str(len(flattened_list)) + " occurrences of the string \n" + "Here's the list \n"
+    prepare_string = "Found in " + str(len(occurrences_list)) + " files \n" + "Found " + str(len(flattened_list)) + " occurrences of the string \n" + "Here's the list \n"
     print(prepare_string)
     print(*flattened_list, sep=' \n')
     
