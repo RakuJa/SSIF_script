3af33c98f360471b7b5ae45394580b56b4ad26ef
RakuJa <danielegiachetto1999@gmail.com>
2021-04-21 15:50:11 +0200

    Refactor, Optimized a little bit wait times

--- src/web_scraper/amazon_product_search.py
+++ src/web_scraper/amazon_product_search.py
@@ -18,6 +18,9 @@ os.environ['REQUESTS_CA_BUNDLE'] = certifi.where()
 BOT_BLACKLISTED = ["To discuss automated access to Amazon data please contact",
                    "For automated access to price change or offer listing change events"]
 
+TIMEOUT_FOR_GET = 5
+WAIT_BETWEEN_PROXY_SCRAPING = 10
+
 
 def check_status(page):
     """
@@ -49,7 +52,7 @@ def check_status(page):
             return False
 
 
-def iterate_request(url, proxy_pool, session):
+def iterate_request(url, proxy_pool, session) -> requests.models.Response:
     """
     This method will iterate n times the request to the url given in input. with n == len(proxy_pool)
     If the requests goes well it will instantly return the value without iterating further
@@ -69,7 +72,7 @@ def iterate_request(url, proxy_pool, session):
         session.proxies.update(_proxies)
         response = None
         try:
-            response = session.get(url, timeout=10)  # get with 10 seconds timeout
+            response = session.get(url, timeout=TIMEOUT_FOR_GET)  # get with 10 seconds timeout
         except ProxyError as e:
             print("Connection refused by target machine, maybe you tried too much in a short span of time..")
         if check_status(response):
@@ -80,7 +83,12 @@ def iterate_request(url, proxy_pool, session):
     return None
 
 
-def download_page(url):
+def download_page(url) -> requests.models.Response:
+    """
+    Method that will try on downloading a page until it succeeded
+    :param url: url to page to download
+    :return: downloaded page as a request.get return obj
+    """
     session = requests.Session()
     while True:
         proxies = get_list_of_proxy_scraper.get_proxies()
@@ -90,9 +98,47 @@ def download_page(url):
                 return response_page
             else:
                 print("Cannot connect... scraping a new proxy list..")
-                time.sleep(15)
+                time.sleep(WAIT_BETWEEN_PROXY_SCRAPING)
         except Exception as e:
             print(e)
+
+
+def scrape_html(page) -> dict:
+    """
+    This method will get name, price and currency tag from html
+    :param page: the html page to scrape, already downloaded
+    :return: return dictionary with item name as a key and price and currency in a vector as value
+    """
+    soup = BeautifulSoup(page.content, 'html.parser')  # This will contain the html page
+
+    # gets item name list
+    item_found = soup.findAll("span", {"class": "a-size-base-plus a-color-base a-text-normal"})
+    # gets item price list
+    price_found = soup.findAll("span", {"class": "a-price-whole"})
+    # gets item currency list
+    currency_found = soup.findAll("span", {"class": "a-price-symbol"})
+    item_dictionary = {}
+    for (curr_name, curr_price, curr_currency) in zip(item_found, price_found, currency_found):
+        # gets name, price and currency strings
+        name = curr_name.get_text()
+        price = curr_price.get_text()
+        currency = curr_currency.get_text()
+        # put the value in a dictionary, name is a key, [price, currency] a value as a vector
+        item_dictionary[name] = [price, currency]
+    return item_dictionary
+
+
+def print_dictionary(dictionary) -> None:
+    """
+    Prints a dictionary row by row as follows, key : value
+    :param dictionary: dictionary to print out
+    :return: None
+    """
+    for item in dictionary:
+        tmp_vector = dictionary[item]
+        currency = tmp_vector.pop()
+        price = tmp_vector.pop()
+        print(item + " : " + price + " " + currency)
 
 
 def start():
@@ -107,28 +153,9 @@ def start():
         full_url = BASE_URL_LIST[0] + result_string
 
         page = download_page(full_url)
-        print(page)
-        soup = BeautifulSoup(page.content, 'html.parser')   # This will contain the html page
+        item_dictionary = scrape_html(page)
+        print_dictionary(item_dictionary)
 
-        item_found = soup.findAll("span", {"class": "a-size-base-plus a-color-base a-text-normal"})
-        price_found = soup.findAll("span", {"class": "a-price-whole"})
-        currency_found = soup.findAll("span",{"class": "a-price-symbol"})
-        print("\n")
-        product_found_list = []
-        item_dictionary={}
-        for (curr_name, curr_price, curr_currency) in zip(item_found, price_found, currency_found):
-            #product_found_list.append()
-            name = curr_name.get_text()
-            price = curr_price.get_text()
-            currency = curr_currency.get_text()
-            item_dictionary[name] = [price, currency]
-            # print(name + " : " + price + " " + currency)
-
-        for amazon_item in item_dictionary:
-            tmp_vector = item_dictionary[amazon_item]
-            currency = tmp_vector.pop()
-            price = tmp_vector.pop()
-            print(amazon_item + " : " + price + " " + currency)
         user_input = input("Search for another product? Y/N : ")
         if user_input.upper() == "N":
             sys.exit()
