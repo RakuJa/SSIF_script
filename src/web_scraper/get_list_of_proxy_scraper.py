e7dad46508af35907371d22b6763604a621decb2
RakuJa <danielegiachetto1999@gmail.com>
2021-04-21 15:49:40 +0200

    Increased to 20 proxy list to scrape

--- src/web_scraper/get_list_of_proxy_scraper.py
+++ src/web_scraper/get_list_of_proxy_scraper.py
@@ -1,13 +1,14 @@
 import requests
 from lxml.html import fromstring
 
+NUMBER_OF_PROXY_ROWS = 20
 
 def get_proxies():
     url = 'https://free-proxy-list.net/'
     response = requests.get(url)
     parser = fromstring(response.text)
     proxies = set()
-    for i in parser.xpath('//tbody/tr')[:10]:
+    for i in parser.xpath('//tbody/tr')[:NUMBER_OF_PROXY_ROWS]:
         if i.xpath('.//td[7][contains(text(),"yes")]'):
             # Grabbing IP and corresponding PORT
             proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
