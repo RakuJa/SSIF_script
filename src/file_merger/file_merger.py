82bd5fc63642f6c7b174e4fbf2bfbc39dc331731
Daniele Giachetto <daniele.giachetto@zextras.com>
2022-11-13 21:35:00 +0100

    feat: add some static analysis, refactor file_merger.py bad code, fix readme runtime error

--- src/file_merger/file_merger.py
+++ src/file_merger/file_merger.py
@@ -1,33 +1,65 @@
+from pathlib import Path
+
 from PyPDF2 import PdfFileMerger
+from pathvalidate import validate_filepath, ValidationError
 import os
 
 
-def check_file_validity(file_name) -> bool:
-    with open(file_name, 'r', encoding='utf-8'):
-        return True
-    return False
-
-
-def remove_duplicates(list_to_clean_up) -> bool:
-    clean_list = list(set(list_to_clean_up))
-    for item in clean_list:
-        check_file_validity(item)
-    return clean_list
-
-
-def take_file_input() -> list:
+def take_file_input() -> set:
     keep_taking_input = True
-    file_list = []
+    file_list: set = set()
     while keep_taking_input:
-        file_name = input("Enter file path, Empty line to stop input ")
-        if file_name == "ALL":
-            file_list = [a for a in os.listdir() if a.endswith(".pdf")]
-        elif file_name == "":
+        file_path = input(
+            "Enter file path, Empty line to stop input, '.' for all pdfs in current dir "
+        )
+        if not file_path:
             keep_taking_input = False
         else:
-            file_list.append(file_name)
-    file_list = remove_duplicates(file_list)
+            if validate_file_path(file_path):
+                if Path(file_path).is_file():
+                    file_list.add(file_path)
+                elif Path(file_path).is_dir():
+                    file_list.update(take_all_files_in_given_path(file_path))
+
     return file_list
+
+
+def take_all_files_in_given_path(
+    dir_path: str = "", filter_for_extension: str = "pdf"
+) -> set:
+    return {
+        os.path.join(dir_path, file)
+        for file in os.listdir(dir_path)
+        if file.endswith(filter_for_extension)
+    }
+
+
+def validate_file_path(file_path: str) -> bool:
+    """
+    Checks for file path formal validity and then existence
+    """
+    try:
+        validate_filepath(file_path, platform="auto")
+        if not Path(file_path).exists():
+            raise ValidationError(f"The given file does not exist: {file_path}")
+        return True
+    except ValidationError as e:
+        print(f"\nEncountered an error while parsing the given path: {e}")
+        return False
+
+
+def take_output_file_name() -> str:
+    keep_taking_input = True
+    while keep_taking_input:
+        file_name = input("Enter output file path, Empty line for default name ")
+        if not file_name:
+            return "result.pdf"
+        else:
+            try:
+                validate_filepath(file_name, platform="auto")
+                return file_name if file_name.endswith(".pdf") else file_name + ".pdf"
+            except ValidationError as e:
+                print(f"\nEncountered an error while parsing the given path: {e}")
 
 
 def start():
@@ -35,5 +67,5 @@ def start():
     merger = PdfFileMerger()
     for pdf in file_list:
         merger.append(pdf)
-    merger.write("result.pdf")
+    merger.write(take_output_file_name())
     merger.close()
--- src/main.py
+++ src/main.py
@@ -1,40 +1,53 @@
 import os
 import sys
+import interface
+import markdown
+
 from file_merger import file_merger
 from SSIF_script import SSIF_script
 from src.web_scraper import amazon_product_search, check_site_differences
 from web_scraper import github_contribution_counter as github_contrib_counter
-import interface
+
+from bs4 import BeautifulSoup
+from pathlib import Path
+
+
+def get_readme_full_path() -> str:
+    """
+    Method used to get the readme full path, if the path will ever change
+    modify this method or just don't call the readme reader function
+    """
+    return os.path.join(Path().resolve().parent, "README.md")
 
 
 def print_readme():
-    import markdown
-    from bs4 import BeautifulSoup
-
-    html = markdown.markdown(open("README.md").read())
-    x = BeautifulSoup(html, features="html.parser").findAll(text=True)
+    html: str = ""
+    with open(get_readme_full_path()) as readme_file:
+        html = markdown.markdown(readme_file.read())
+    parsed_html = BeautifulSoup(html, features="html.parser").findAll(text=True)
     print("\n")
-    for line in x:
-        print(line)
-    print("\n")
+    for line in parsed_html:
+        if line and line != "\n":
+            print(line)
 
 
 def print_instructions():
-    text = "[1] Read Me \n" \
-           + "[2] File merger \n" \
-           + "[3] Search string in file \n" \
-           + "[4] Github contribution counter \n" \
-           + "[5] Amazon product finder \n" \
-           + "[6] Site differences \n" \
-           + "[7] Credits" \
-           + "[8] Instructions  " \
-           + "[9] Exit \n "
+    text = (
+        "[1] Read Me \n"
+        + "[2] File merger \n"
+        + "[3] Search string in file \n"
+        + "[4] Github contribution counter \n"
+        + "[5] Amazon product finder \n"
+        + "[6] Site differences \n"
+        + "[7] Credits"
+        + "[8] Instructions  "
+        + "[9] Exit \n "
+    )
     print(interface.bordered(text))
 
 
 def require_input():
-    choice = input("Enter Your Choice in the Keyboard [1,2,3,5,6,7,8,9] : ")
-    return choice.upper()
+    return input("Enter Your Choice in the Keyboard [1,2,3,5,6,7,8,9] : ").upper()
 
 
 if __name__ == "__main__":
@@ -50,21 +63,21 @@ if __name__ == "__main__":
 
     while True:
         choice = require_input()
-        if choice == '1' or choice == 'READ':
+        if choice == "1" or choice == "READ":
             print_readme()
-        elif choice == '2' or choice == 'MERGER':
+        elif choice == "2" or choice == "MERGER":
             file_merger.start()
-        elif choice == '3' or choice == 'SSIF' or choice == 'SEARCH':
+        elif choice == "3" or choice == "SSIF" or choice == "SEARCH":
             SSIF_script.start()
-        elif choice == '4' or choice == 'CONTRIBUTION' or choice == 'GITHUB':
+        elif choice == "4" or choice == "CONTRIBUTION" or choice == "GITHUB":
             github_contrib_counter.start()
-        elif choice == '5' or choice == 'AMAZON':
+        elif choice == "5" or choice == "AMAZON":
             amazon_product_search.start()
-        elif choice == '6':
+        elif choice == "6":
             check_site_differences.start()
-        elif choice == '7' or choice == 'CREDITS':
+        elif choice == "7" or choice == "CREDITS":
             print("https://github.com/RakuJa")
-        elif choice == '8' or choice == 'INSTRUCTIONS':
+        elif choice == "8" or choice == "INSTRUCTIONS":
             print_instructions()
-        elif choice == '9' or choice == 'EXIT':
+        elif choice == "9" or choice == "EXIT":
             sys.exit()
